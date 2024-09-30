4b37a0dcff81710a35c65ad38e0b38205742e547
BuddhadebKoner <jeetkoner36@gmail.com>
2024-09-30 19:57:20 +0530

    all pages pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import './globals.css'
 // routes components
 import SigninForm from './_auth/forms/SignInForm'
 import SignupForm from './_auth/forms/SignupForm'
-import { Home } from './_root/pages'
 import AuthLayout from './_auth/AuthLayout'
 import RootLayout from './_root/RootLayout'
 
 import { Toaster } from "@/components/ui/toaster"
+// all pages
+import { AllUsers, CreatePost, Explore, Home, PostDetails, Profile, Saved, UpdatePost, UpdateProfile } from './_root/pages'
 
 
 function App() {
@@ -22,6 +23,14 @@ function App() {
             {/* private routes */}
             <Route element={<RootLayout />}>
                <Route index element={<Home />} />
+               <Route path='/explore' element={<Explore />} />
+               <Route path='/all-users' element={<AllUsers />} />
+               <Route path='/saved' element={<Saved />} />
+               <Route path='/create-post' element={<CreatePost />} />
+               <Route path='/update-post/:id' element={<UpdatePost />} />
+               <Route path='/post/:id' element={<PostDetails />} />
+               <Route path='/profile/:id*' element={<Profile />} />
+               <Route path='/update-profile/:id*' element={<UpdateProfile />} />
             </Route>
          </Routes>
          <Toaster />
